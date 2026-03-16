4cd733d0ef3e1bc4147dd3f34965a094bfc91e1b
Ivan Chabanov <firstchabanov@gmail.com>
2026-03-17 00:19:48 +0300

    Move inference to another file

---
+++ lib/inference.dart
@@ -0,0 +1,50 @@
+import 'dart:ffi';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:ffi/ffi.dart';
+import 'package:llama_inference/llama_bindings.g.dart';
+
+Future<String> inference(String prompt) async {
+  final modelPath = "/Users/chabanovz/prog/flutter/experiments/llama_inference/models/qwen2.5-0.5b-instruct-q5_k_m.gguf".toNativeUtf8();
+  final llamaHandlePtr = llama_dart_create(modelPath.cast<Char>(), 2048, 4);
+  if (llamaHandlePtr == nullptr) {
+    print('LLAMA FAILED TO REGISTER. EXITING');
+    exit(1);
+  }
+  malloc.free(modelPath);
+
+  // Generate.
+  final prompt = "Hello! How are you?".toNativeUtf8().cast<Char>();
+  final outputBuf = malloc<Uint8>(16 * 1024);
+
+  final written = llama_dart_generate(
+    llamaHandlePtr,
+    prompt,
+    128 * 2,
+    outputBuf.cast<Char>(),
+    16 * 1024,
+  );
+
+  // print('WRITTEN RESULT: $written');
+
+  malloc.free(prompt);
+
+  if (written < 0) {
+    malloc.free(outputBuf);
+    llama_dart_destroy(llamaHandlePtr);
+    print('Generation failed with code $written');
+    exit(1);
+  }
+
+  final bytes = outputBuf.asTypedList(written);
+  final outputString = utf8.decode(bytes);
+  // print('OUTPUT: $outputString');
+
+  malloc.free(outputBuf);
+
+  // Destroy the shit.
+  llama_dart_destroy(llamaHandlePtr);
+
+  return outputString;
+}
